3bed2398e1453dc18741bcb6224aafcfa5daba27
Wayan <wayan@tix.com.au>
2023-03-17 21:02:40 +0800

    Add fullname field as custom properties

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
           'first_name' => $this->first_name,
           'last_name' => $this->last_name,
           'birthday'=> $this->birthday,
-          'location'=> $this->location
+          'location'=> $this->location,
         ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,4 +14,13 @@ class User extends Model
         'id', 'first_name', 'last_name', 'birthday', 'location',
     ];
 
+    protected $appends = ['full_name'];
+
+    // The function name will need to start with `get`and ends with `Attribute`
+// with the attribute field in-between in camel case.
+    public function getFullNameAttribute() // notice that the attribute name is in CamelCase.
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
 }
